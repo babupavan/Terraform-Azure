8311d465102a041d47a0665697674d1526d951e5
SyedTousif <stousif1020@gmail.com>
2024-11-07 17:49:36 +0530

    Update main.tf

--- Day 6-7 (modules)/Root/modules/nsg/main.tf
+++ Day 6-7 (modules)/Root/modules/nsg/main.tf
@@ -1,5 +1,13 @@
-resource "azurerm_network_security_group" "nsg" {
-  name = var.nsg_name
-  resource_group_name = var.resource_group_name
+# Resource Group Creation
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
   location = var.location
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  depends_on = [azurerm_resource_group.rg]
 }
