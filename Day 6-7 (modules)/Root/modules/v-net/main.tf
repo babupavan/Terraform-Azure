bcfcd1b5796c2b30e6c422de512c7dd0796dea37
SyedTousif <stousif1020@gmail.com>
2024-11-07 17:51:02 +0530

    Update main.tf

--- Day 6-7 (modules)/Root/modules/v-net/main.tf
+++ Day 6-7 (modules)/Root/modules/v-net/main.tf
@@ -1,6 +1,19 @@
-resource "azurerm_virtual_network" "vnet" {
-  name = var.vnet_name
-  address_space = [ var.vnet_address_prefix ]
+# main.tf file (for root module)
+
+# Resource Group Creation
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
   location = var.location
-  resource_group_name = var.resource_group_name
 }
+
+# Virtual Network Creation
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = [var.vnet_address_prefix]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  # Ensure VNet creation after RG creation
+  depends_on = [azurerm_resource_group.rg]
+}
+  
